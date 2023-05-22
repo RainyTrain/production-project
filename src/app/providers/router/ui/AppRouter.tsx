ebfc621d6cbbe2b883e4c14bf87cae03aa4739a6
Adrian Senkevich <adrianscorp@gmail.com>
2023-05-22 20:36:00 +0200

    redirecting from non-existing page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,15 +1,20 @@
-import { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from 'shared';
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared";
+import { PageLoader } from "widgets/PageLoader/PageLoader";
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ path, element }) => (
-          <Route key={path} path={path} element={<div className='page-wrapper'>{element}</div>} />
-        ))}
-      </Routes>
-    </Suspense>
-  );
+  <Suspense fallback={<PageLoader />}>
+    <Routes>
+      {Object.values(routeConfig).map(({ path, element }) => (
+        <Route
+          key={path}
+          path={path}
+          element={<div className="page-wrapper">{element}</div>}
+        />
+      ))}
+    </Routes>
+  </Suspense>
+);
 
 export default AppRouter;
