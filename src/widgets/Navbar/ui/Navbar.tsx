f29c8c0b32e768cb6004a5894514fcf71dfa4e95
Adrian Senkevich <adrianscorp@gmail.com>
2023-05-27 14:27:56 +0200

    fixed warnings in ESLint and build errors in tsconfig

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,24 @@
-import { AppLink, classNames } from 'shared';
-import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import cls from './Navbar.module.scss';
+import { AppLink, classNames } from "shared";
+import { AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        <AppLink to="/" theme={AppLinkTheme.PRIMARY} className={cls.mainLink}>
-          Main
-        </AppLink>
-        <AppLink to="/about" theme={AppLinkTheme.PRIMARY} className={cls.mainLink}>
-          About
-        </AppLink>
-      </div>
+  <div className={classNames(cls.Navbar, {}, [className])}>
+    <div className={cls.links}>
+      <AppLink to="/" theme={AppLinkTheme.PRIMARY} className={cls.mainLink}>
+        Main
+      </AppLink>
+      <AppLink
+        to="/about"
+        theme={AppLinkTheme.PRIMARY}
+        className={cls.mainLink}
+      >
+        About
+      </AppLink>
     </div>
-  );
+  </div>
+);
