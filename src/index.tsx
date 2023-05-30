13b35c40dbd17f6d8956ff59ce804f330fdd0e5b
Adrian Senkevich <adrianscorp@gmail.com>
2023-05-30 23:40:10 +0200

    fixed issue with github actions v5

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "app/App";
 import { ThemeProvider } from "shared/ui/ThemeProvider";
 import "shared/config/i18n/i18n";
 import { ErrorBoundary } from "app/providers/ErrorBoundary";
+import App from "app/app";
 
 render(
   <BrowserRouter>
