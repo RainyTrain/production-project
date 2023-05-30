37535517b7e070648b9c6d5d6bd3febb33803a11
Adrian Senkevich <adrianscorp@gmail.com>
2023-05-30 23:50:41 +0200

    fixed dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,20 +7,29 @@ import { BuildOptions } from "./types/config";
 export const buildPlugins = ({
   paths,
   isDev,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new HtmlWebpackPlugin({
-    template: paths.html,
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: "css/[name].[contenthash].css",
-    chunkFilename: "css/[name].[contenthash].css",
-  }),
-  new webpack.DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev),
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-  }),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css",
+      chunkFilename: "css/[name].[contenthash].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      })
+    );
+  }
+
+  return plugins;
+};
