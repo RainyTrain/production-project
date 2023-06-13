9b833f5182a211b3e64933d4539939748d8b947a
Adrian Senkevich <adrianscorp@gmail.com>
2023-06-13 22:37:28 +0200

    fixed husky pre-commit hook

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from "shared";
 import { Modal } from "shared/ui/Modal/Modal";
 import { LoginForm } from "../LoginForm/LoginForm";
-import cls from "./LoginModal.module.scss";
 
 interface LoginModalProps {
   className?: string;
@@ -14,7 +13,7 @@ export const LoginModal = ({ className, onClose, isOpen }: LoginModalProps) => (
     isOpen={isOpen}
     onClose={onClose}
     lazy
-    className={classNames(cls.LoginModal, {}, [className])}
+    className={classNames("", {}, [className])}
   >
     <LoginForm />
   </Modal>
