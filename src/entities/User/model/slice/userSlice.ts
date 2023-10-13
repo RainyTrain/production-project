db088d8d2a36082c30179c7e735f1c2b26d61ce9
Adrian Senkevich <adrianscorp@gmail.com>
2023-10-13 13:26:36 +0200

    created HOC buildSlice to avoid dispatching actions in components

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,15 +1,19 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit";
 import { USE_LOCALSTORAGE_KEY } from "shared/const/localstorage";
+import { buildSlice } from "shared/lib/store/buildSlice";
 import { User, UserSchema } from "../types/userSchema";
 
 const initialState: UserSchema = { _inited: false };
 
-export const userSlice = createSlice({
+export const userSlice = buildSlice({
   name: "user",
   initialState,
   reducers: {
     setAuthData: (state, action: PayloadAction<User>) => {
       state.authData = action.payload;
+    },
+    sayHi: (state) => {
+      console.log(state.authData?.username, "Hi");
     },
     initAuthData: (state) => {
       const user = localStorage.getItem(USE_LOCALSTORAGE_KEY);
@@ -27,3 +31,4 @@ export const userSlice = createSlice({
 
 export const { actions: userActions } = userSlice;
 export const { reducer: userReducer } = userSlice;
+export const { useActions: userUseActions } = userSlice;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,22 @@
 import { RatingCard } from "entities/Rating";
+// eslint-disable-next-line rainytrain-fsd-plugin/public-api-imports
+import { userUseActions } from "entities/User/model/slice/userSlice";
+import { useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Page } from "widgets/Page";
 
 const MainPage = () => {
   const { t } = useTranslation();
+
+  const { sayHi } = userUseActions();
+
+  const hadleGreet = useCallback(() => {
+    sayHi();
+  }, [sayHi]);
+
+  useEffect(() => {
+    hadleGreet();
+  }, [hadleGreet]);
 
   return (
     <Page>
